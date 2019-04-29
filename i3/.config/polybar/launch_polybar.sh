246a25f787c41fca86bb94719088ab141b11271b
Josh Robson Chase <josh@robsonchase.com>
2019-04-29 11:31:28 -0400

    Make Polybar work with multiple monitors

---
+++ i3/.config/polybar/launch_polybar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+MONITORS=$(polybar -m | awk 'BEGIN{FS=":"};{print $1}' | tac)
+
+for monitor in $MONITORS
+do
+	MONITOR=$monitor polybar -r i3 &
+done
