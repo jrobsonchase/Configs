b9ffa236838102a0eac5530f6204ae305ff6e9d7
Josh Robson Chase <josh@robsonchase.com>
2019-05-02 20:45:24 -0400

    Update polybar launcher

--- i3/.config/polybar/launch_polybar.sh
+++ i3/.config/polybar/launch_polybar.sh
@@ -6,9 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITORS=$(polybar -m | awk 'BEGIN{FS=":"};{print $1}' | tac)
+MONITORS=$(polybar -m | grep -v XRandR | awk 'BEGIN{FS=":"};{print $1}')
 
 for monitor in $MONITORS
 do
 	MONITOR=$monitor polybar -r i3 &
+	sleep 0.5
 done
