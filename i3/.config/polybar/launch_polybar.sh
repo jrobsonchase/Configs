a5df119738b9a10ab919e8c3648fdf8aaf555ab5
Josh Robson Chase <josh@robsonchase.com>
2019-05-07 08:45:18 -0400

    Update polybar config

--- i3/.config/polybar/launch_polybar.sh
+++ i3/.config/polybar/launch_polybar.sh
@@ -6,7 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITORS=$(polybar -m | grep -v XRandR | awk 'BEGIN{FS=":"};{print $1}' | tac)
+MONITORS=$(polybar -m | grep -v XRandR | cut -d : -f 1 | tac)
+
+POLYBAR_ETH=$(ip link | grep -P '\d: en' | cut -d : -f 2 | tr -d ' ')
+export POLYBAR_ETH1=$(echo $POLYBAR_ETH | cut -d ' ' -f 1)
+export POLYBAR_ETH2=$(echo $POLYBAR_ETH | cut -d ' ' -f 2)
+export POLYBAR_WLAN=$(ip link | grep -P '\d: wl' | cut -d : -f 2 | tr -d ' ')
 
 for monitor in $MONITORS
 do
