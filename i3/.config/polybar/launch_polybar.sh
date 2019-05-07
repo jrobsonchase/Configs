6e5a40aae55c3e691f55a89c607c35fc1740b9e0
Josh Robson Chase <josh@robsonchase.com>
2019-05-07 08:51:19 -0400

    Better polybar interface handling

--- i3/.config/polybar/launch_polybar.sh
+++ i3/.config/polybar/launch_polybar.sh
@@ -8,9 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 MONITORS=$(polybar -m | grep -v XRandR | cut -d : -f 1 | tac)
 
-POLYBAR_ETH=$(ip link | grep -P '\d: en' | cut -d : -f 2 | tr -d ' ')
-export POLYBAR_ETH1=$(echo $POLYBAR_ETH | cut -d ' ' -f 1)
-export POLYBAR_ETH2=$(echo $POLYBAR_ETH | cut -d ' ' -f 2)
+INTERFACES=$(ip link | grep -P '\d: en' | cut -d : -f 2 | tr -d ' ')
+ETH_CTR=1
+for iface in $INTERFACES; do
+	eval "export POLYBAR_ETH${ETH_CTR}=$iface"
+	let ETH_CTR+=1
+done
+
 export POLYBAR_WLAN=$(ip link | grep -P '\d: wl' | cut -d : -f 2 | tr -d ' ')
 
 for monitor in $MONITORS
